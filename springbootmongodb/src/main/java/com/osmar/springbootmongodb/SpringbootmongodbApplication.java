90a2f52ecdeb8af9dd2b74a86e93587bf7ca0500
Osmarhair <89876986+Osmarhair@users.noreply.github.com>
2024-10-13 05:20:21 -0300

    first commit

---
+++ springbootmongodb/src/main/java/com/osmar/springbootmongodb/SpringbootmongodbApplication.java
@@ -0,0 +1,24 @@
+package com.osmar.springbootmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+
+@RestController
+@SpringBootApplication
+public class SpringbootmongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootmongodbApplication.class, args);
+	}
+
+	@RequestMapping("/hello")
+	public String Hello() {
+		return "olá";
+	}
+
+}
---
+++ springbootmongodb/src/test/java/com/osmar/springbootmongodb/SpringbootmongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osmar.springbootmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootmongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
